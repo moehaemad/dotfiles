1f27c5dad68bebcf64a9daa179193478ce715529
Hassan Ali <muhammad.ali.72.27@gmail.com>
2023-01-23 20:27:47 -0500

    Start of recreating desktop with single command

--- InstallDesktop.sh
+++ InstallDesktop.sh
@@ -20,7 +20,14 @@ obs-studio,
 ffmpeg,
 feh,
 maim,
-compton
+compton,
+figlet,
+lolcat
+)
+
+#Download it, don't download, doesn't really matter :/
+snap_packages = (
+gotop
 )
 
 sudo add-apt-repository ppa:obsproject/obs-studio
@@ -29,3 +36,14 @@ for package in '${software[@]}'; do
 	#-f to force reinstall
 	sudo apt install -y -f "$package" || true
 done
+
+default_chrome='Default'
+arm_chrome='Profile 1'
+
+#Move my quirks to local executable
+echo -e "#!/bin/bash\n\ngoogle-chrome --profile-directory="$default_chrome" --new-window" >> mob && chmod +x mob && sudo mv mob /usr/bin/
+
+echo -e "#!/bin/bash\n\ngoogle-chrome --profile-directory="$arm_chrome" --new-window" >> mobb && chmod +x mobb && sudo mv mobb /usr/bin/
+
+mkdir ~/.config/compton && ln -s compton/compton.conf ~/.config/compton/compton.conf
+
